b71ddb60f432873bcca77f1a73f850ad36c28b9d
RyanDerr <JamesRDerr@gmail.com>
2025-03-04 16:29:09 -0600

    Add Server Side gRPC APIs For Set & Get

--- pkg/service/cache-api/get.go
+++ pkg/service/cache-api/get.go
@@ -3,10 +3,32 @@ package service
 import (
 	"context"
 
+	getcmd "github.com/bgajjala8/go-cache/pkg/get"
 	pb "github.com/bgajjala8/go-cache/proto-public/go"
+	validation "github.com/go-ozzo/ozzo-validation"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// TODO: Implement the Get method of the Service
-func (s *Service) Get(context.Context, *pb.GetRequest) (*pb.GetResponse, error) {
-	return nil, nil
+func validateGetRequest(req *pb.GetRequest) error {
+	return validation.ValidateStruct(req, validation.Field(&req.Key, validation.Required))
+}
+
+func (s *Service) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
+	err := validateGetRequest(req)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "validation error: %s", err.Error())
+	}
+
+	resp, err := getcmd.Get(s.cache, req.GetKey())
+	if err != nil {
+		switch err {
+		case getcmd.ErrKeyNotFound:
+			return nil, status.Errorf(codes.NotFound, "key not found: %s", req.GetKey())
+		default:
+			return nil, status.Errorf(codes.Internal, "error getting key: %s", err.Error())
+		}
+	}
+
+	return &pb.GetResponse{Key: req.GetKey(), Value: resp}, nil
 }
